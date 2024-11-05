53d32dffe821979f5dcde3dfbefd11d9999c12da
Nitrorex17 <115041681+Nitrorex17@users.noreply.github.com>
2024-11-05 03:46:01 -0500

    test

--- src/app/results/page.tsx
+++ src/app/results/page.tsx
@@ -52,14 +52,12 @@ function CourseDropdown({ course }: { course: Course }) {
 export default function Results() {
   const params = useParams<{subject: string, course: string}>()
 
-  
-
   return (
     <div className="max-w-md mx-auto bg-gray-100 min-h-screen">
       <h1 className="text-2xl font-bold text-center mb-6">{params.subject}</h1>
       <main className="p-4">
         { Courses.filter(
-          (course : Course) => (['', course.subject].includes(params.subject) && ['', course.number].includes(params.course))
+          (course : Course) => (["any", course.subject].includes(params.subject) && ["any", course.number].includes(params.course))
         ).map(
           (course : Course) => (<CourseDropdown course={course}/>)
         )}
