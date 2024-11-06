16111955d8183345b74fb3cc91386abccfd9d56e
Nitrorex17 <115041681+Nitrorex17@users.noreply.github.com>
2024-11-05 20:10:12 -0500

    test

--- src/components/meta/context.tsx
+++ src/components/meta/context.tsx
@@ -3,15 +3,15 @@
 import { createContext, useContext, useState, ReactNode } from 'react'
 import { Account } from "@/components/ui/data"
 
-interface UserContextType {
+interface AccountContextType {
     user: Account | null
     setUser: (user: Account | null) => void
 }
 
-export const AccountContext = createContext()
+export const AccountContext = createContext<AccountContextType | undefined>(undefined)
 
 export function AccountProvider({children} : {children : ReactNode}) {
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState<Account, null>(null)
 
   return (
     <AccountContext.Provider value={{user, setUser}}>
