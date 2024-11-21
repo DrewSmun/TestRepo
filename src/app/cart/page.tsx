c49a59305edb01116e1e2fc67ca976d450b9d881
Nitrorex17 <115041681+Nitrorex17@users.noreply.github.com>
2024-11-20 21:02:33 -0500

    szdfxgchvj

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -103,6 +103,7 @@ export default function Cart() {
     let query = `MATCH (p:Profile {CWID: ${user}}) RETURN p.holdNotification`
     let response = await read(query)
 
+    console.log(query)
     console.log(JSON.stringify(response, null, 4))
 
     if (response[0].hold) {
